804a8b307eb8b5ce9fcfcf4f8199dc68ab73ba7d
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-16 20:42:55 +0800

    modify styles for QuoteSection and SkillSection components

--- components/QuoteSection.tsx
+++ components/QuoteSection.tsx
@@ -1,13 +1,13 @@
 import { NextPage } from 'next';
-
 interface Props {
 	text: string;
 }
 
 const QuoteSection: NextPage<Props> = ({ text }) => {
 	return (
-		<section className="grid place-items-center bg-gray-800 min-h-[15rem] py-10 text-gray-50 ">
-			<h3 className="w-4/5 max-w-3xl text-lg font-bold">{text}</h3>
+		<section className="grid gap-y-8 place-items-center bg-gray-800 min-h-[15rem] pt-20 pb-48 text-gray-50 ">
+			<h2 className="text-5xl font-bold">JavaScript</h2>
+			<h3 className="w-3/4 max-w-3xl text-lg font-bold">{text}</h3>
 		</section>
 	);
 };
--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 const SkillSection: NextPage = () => {
 	return (
 		<section className="grid place-items-center py-10">
-			<div className="grid gap-y-8 justify-center w-3/4 max-w-5xl min-h-[24rem] bg-gray-50 py-10 text-center rounded-3xl xl:grid-cols-3">
+			<div className="grid gap-y-8 justify-center w-3/4 max-w-5xl min-h-[24rem] -mt-40 bg-gray-50 py-10 text-center rounded-3xl xl:grid-cols-3">
 				<div>
 					<h1>Title 1</h1>
 					<ul>
