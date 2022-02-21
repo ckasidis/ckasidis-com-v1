6f5d4ace7c282405254e8212da486cdd14f2b4d7
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-22 00:43:52 +0800

    add req.body to email content in ses.js

--- pages/api/ses.js
+++ pages/api/ses.js
@@ -1,27 +1,15 @@
 import { sesClient } from '../../lib/sesClient';
 import { SendEmailCommand } from '@aws-sdk/client-ses';
 
-// AWS.config.update({ region: process.env.AWS_REGION });
-
-// const creds = new AWS.Credentials({
-// 	accessKeyId: process.env.SES_ACCESS_KEY_ID,
-// 	secretAccessKey: process.env.SES_SECRET_ACCESS_KEY,
-// });
-
-// const ses = new AWS.SES({
-// 	credentials: creds,
-// });
-
 const sendEmail = async (req, res) => {
 	var params = {
 		Destination: {
 			/* required */
 			CcAddresses: [
-				'test001@ckasidis.com',
 				/* more items */
 			],
 			ToAddresses: [
-				'me@ckasidis.com',
+				'info@ckasidis.com',
 				/* more items */
 			],
 		},
@@ -31,21 +19,25 @@ const sendEmail = async (req, res) => {
 				/* required */
 				Html: {
 					Charset: 'UTF-8',
-					Data: 'HTML_FORMAT_BODY',
+					Data: `
+					<h3>Full Name: ${req.body.fullname}</h3>
+					<h3>Email: ${req.body.email}</h3>
+					<p>${req.body.message}</p>`,
 				},
 				Text: {
 					Charset: 'UTF-8',
-					Data: 'TEXT_FORMAT_BODY',
+					Data: `Full Name: ${req.body.fullname}\n
+					Email: ${req.body.email}\n
+					Message: ${req.body.message}`,
 				},
 			},
 			Subject: {
 				Charset: 'UTF-8',
-				Data: 'Test email',
+				Data: `Contact from Website: ${req.body.subject}`,
 			},
 		},
 		Source: 'no-reply@ckasidis.com' /* required */,
 		ReplyToAddresses: [
-			'info@ckasidis.com',
 			/* more items */
 		],
 	};
@@ -59,18 +51,6 @@ const sendEmail = async (req, res) => {
 	}
 
 	return res.status(200).json({ error: '' });
-
-	// ses.sendEmail(params, function (err, data) {
-	// 	if (err) {
-	// 		console.log(err, err.stack);
-	// 		res.status(err.statusCode || 500).json({ error: err.message });
-	// 	}
-	// 	// an error occurred
-	// 	else {
-	// 		console.log(data);
-	// 		res.status(200).json({ error: '' });
-	// 	} // successful response
-	// });
 };
 
 export default sendEmail;
